e602faef60c9d5200b21139910b452e2dbff0694
Mahaprasad Dehury <99078610+heymde0x541@users.noreply.github.com>
2022-03-28 20:18:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+document.documentElement.style.setProperty("--text-color", "#F2CDCD");
+
 function light() {
     document.body.classList.toggle("light-mode");
     document.getElementById('id0').classList.toggle("light-mode-div1");
     document.getElementById('id1').classList.toggle("light-mode-div0");
     document.getElementById('id2').classList.toggle("light-mode-div1");
+    
+    if (document.documentElement.style.getPropertyValue('--text-color') == "#F2CDCD"){
+        document.documentElement.style.setProperty("--text-color", "#161320");
+        document.getElementById('switch').innerHTML = "darktheme";
+    }
+    else {
+        document.documentElement.style.setProperty("--text-color", "#F2CDCD");
+        document.getElementById('switch').innerHTML = "lighttheme";
+    }
 }
